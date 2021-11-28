f78a28a6e87222c3bd7a9feed43c3691f1b5d1e2
sakir tufan <sakir.tufan@familienservice.de>
2021-11-28 19:45:42 +0100

    Display Validation Error To User

--- src/Pages/Signup/UserSignupPage.js
+++ src/Pages/Signup/UserSignupPage.js
@@ -5,45 +5,50 @@ import {signup} from "../../api/apiCalls"
 import signupBg from "../../images/bg-signup.png";
 
 export const UserSignupPage = () => {
+
   const [state, setState] = useState({
     username: "",
     displayName: "",
     password: "",
     passwordRepeat: "",
     pendingApiCall: false,
+    errors:{}
   });
 
   const handleChange = (event) => {
     const { name, value } = event.target;
+    const errors = { ...state.errors};
+    errors[name] = undefined;
+
     setState({
       ...state,
       [name]: value,
+      errors
     });
   };
 
   const onClickSignup = (e) => {
-
     e.preventDefault();
-
     const { username, displayName, password } = state;
-
     const body = {
       username,
       displayName,
       password,
     };
 
-    setState({
+    setState({...state,
       pendingApiCall: true,
     });
 
     signup(body)
     .then((response) => {
-      setState({pendingApiCall: false});
+      setState({...state, pendingApiCall: false});
     })
-    .catch((error)=>{
-        setState({pendingApiCall: false});
-
+    .catch((error)=>{   
+      setState({...state, pendingApiCall: false});
+      if(error.response.data.validationErrors){
+        setState({...state, errors : error.response.data.validationErrors})
+      }     
     })
   };
 
@@ -60,14 +65,17 @@ export const UserSignupPage = () => {
       <section className="signup container d-flex justify-content-center align-items-center">
         <form className="bg-primary text-light">
           <h1 className="text-center mb-3"> Sign Up </h1>
-          <div className="mb-3">
-            <label className="form-label"> Username </label>
+          <div className="mb-3 form-group">
+            <label htmlFor="username" className="form-label"> Username </label>
             <input
-              className="form-control"
+              className={state.errors.username ? 'form-control is-invalid' : 'form-control'}
+              id="username"
               name="username"
+              required
               value={state.username}
               onChange={handleChange}
             />
+             <div className="invalid-feedback">{state.errors.username}</div>
           </div>
           <div className="mb-3">
             <label className="form-label"> Display Name </label>
